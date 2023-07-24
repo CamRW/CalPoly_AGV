3fb935e89d5461fadcb5183f5baca136bfc37967
Cameron Weigel <cameronrweigel@gmail.com>
2023-07-24 13:35:16 -0700

    Revert "Removed Submodules"
    This reverts commit 4db816a70c67eec7c26134590dfb9859cd0e7b82.

--- isaac_ros-dev/src/image_stitcher/image_stitcher/image_stitcher_node.py
+++ isaac_ros-dev/src/image_stitcher/image_stitcher/image_stitcher_node.py
@@ -5,28 +5,28 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-import json
+import message_filters
+import time
 
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
-camera_dict = dict()
 
-with open('/workspaces/isaac_ros-dev/configs/camera_configs/cameras.json') as camera_json:
-    camera_dict = json.load(camera_json)
+
 
 class ImageStitching(Node):
     
     def __init__(self):
         super().__init__("image_stitching")
-        self.publisher_ = self.create_publisher(Image, '/video/stitched_frames', 1)
-        self.subscription = self.create_subscription(Image, '/camera/color/image_raw', self.realsense_callback, 1)
+        self.publisher_ = self.create_publisher(Image, '/video/stitched_frames', 10)
+        self.subscription = self.create_subscription(Image, '/camera/color/image_raw', self.realsense_callback, 2)
         timer_period = 0.03 #seconds
         self.timer = self.create_timer(timer_period, self.stitching_callback)
 
-        self.cap1 = cv2.VideoCapture(camera_dict['/dev/back_fisheye'], cv2.CAP_V4L2) # Fisheye Back
-        self.cap2 = cv2.VideoCapture(camera_dict['/dev/left_fisheye'], cv2.CAP_V4L2) # Left, video6
-        self.cap3 = cv2.VideoCapture(camera_dict['/dev/right_fisheye'], cv2.CAP_V4L2) # Right, video12
+
+        self.cap1 = cv2.VideoCapture("/dev/right_fisheye") # Fisheye Back
+        self.cap2 = cv2.VideoCapture(6) # Left, video6
+        self.cap3 = cv2.VideoCapture(12) # Right, video12
 
 
         self.cap1.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
@@ -56,22 +56,18 @@ class ImageStitching(Node):
         self.realsense_image = cv2.cvtColor(self.realsense_image, cv2.COLOR_RGB2BGR)
         
     def stitching_callback(self):
+        stitched_frames = Image()
+        _, frame1 = self.cap1.read()
+        _, frame2 = self.cap2.read()
+        _, frame3 = self.cap3.read()
+    
+        frame1 = np.pad(frame1, pad_width=((0,0),(640,640),(0,0)))
+        merged_frames = np.concatenate((frame2, self.realsense_image, frame3), axis=1)
+        merged_frames = np.concatenate((merged_frames, frame1), axis=0)
+        merged_frames = cv2.resize(merged_frames, dsize=(720,480), interpolation=cv2.INTER_LINEAR)
+        stitched_frames = self.bridge.cv2_to_imgmsg(merged_frames, 'bgr8')
 
-        try:
-            stitched_frames = Image()
-            _, frame1 = self.cap1.read()
-            _, frame2 = self.cap2.read()
-            _, frame3 = self.cap3.read()
-        
-            frame1 = np.pad(frame1, pad_width=((0,0),(640,640),(0,0)))
-            merged_frames = np.concatenate((frame2, self.realsense_image, frame3), axis=1)
-            merged_frames = np.concatenate((merged_frames, frame1), axis=0)
-            merged_frames = cv2.resize(merged_frames, dsize=(720,480), interpolation=cv2.INTER_LINEAR)
-            stitched_frames = self.bridge.cv2_to_imgmsg(merged_frames, 'bgr8')
-
-            self.publisher_.publish(stitched_frames)
-        except Exception as e:
-            print(e)
+        self.publisher_.publish(stitched_frames)
 
     
 def main(args=None):
